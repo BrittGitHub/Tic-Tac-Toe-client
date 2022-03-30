b6b83ec0cdb5f0f66267548f2255984934933677
BrittGitHub <brittany.cavendish@gmail.com>
2022-03-30 08:44:34 -0400

    started game logic and modified README.md file

--- README.md
+++ README.md
@@ -1,4 +1,23 @@
+# Tic-Tac-Toe
+This application allows you to play Tis-Tac-Toe
+
+# Link
+- https://brittgithub.github.io/Tic-Tac-Toe-client/
+
+# User Stories
+
+   1. As a player, I want to be able to create an account
+   2. As a player, I want to be able to sign in if I have an account
+   3. As a player, I want to be able to sign out of my account
+   4. As a player, I want to be able to start a new tic-tac-to game
+   5. As a player, I want to be able to restart a new tic-tac-to game
+   6. As a player, I want to be able to stop playing tic-tac-to after a game I completed
+   7. As a player, I want to see if `X` or `O` won or if the game was a tie
+
+# Wireframe
+![Wireframe](/public/laptop_wirefram_draft1.jpeg)
+
 #### Resources: 
-- https://stackoverflow.com/questions/4665466/using-an-if-statement-to-check-if-a-div-is-empty - for my check if div is empty
-- https://javascript.plainenglish.io/the-worlds-most-empowering-tic-tac-toe-javascript-tutorial-a889e4c20883 - for switching turn logic
-- 
+- https://stackoverflow.com/questions/4665466/using-an-if-statement-to-check-if-a-div-is-empty
+- https://javascript.plainenglish.io/the-worlds-most-empowering-tic-tac-toe-javascript-tutorial-a889e4c20883 
+- https://www.geeksforgeeks.org/simple-tic-tac-toe-game-using-javascript/
--- app/games/gameevents.js
+++ app/games/gameevents.js
@@ -36,8 +36,6 @@ const gameBoard = {
 
 let currentPlayer = 'X'
 
-const over = false
-
 const onSquareClicked = function (event) {
   const cellIndex = this.getAttribute('data-cell-index')
 
@@ -58,14 +56,50 @@ const onSquareClicked = function (event) {
     } else {
       currentPlayer = 'X'
     };
+
+    const c0 = gameCellsArray[0]
+    const c1 = gameCellsArray[1]
+    const c2 = gameCellsArray[2]
+    const c3 = gameCellsArray[3]
+    const c4 = gameCellsArray[4]
+    const c5 = gameCellsArray[5]
+    const c6 = gameCellsArray[6]
+    const c7 = gameCellsArray[7]
+    const c8 = gameCellsArray[8]
+
+    console.log(gameCellsArray)
+
+    if ((c0 === 'X' && c1 === 'X' && c2 === 'X') ||
+    (c3 === 'X' && c4 === 'X' && c5 === 'X') ||
+    (c6 === 'X' && c7 === 'X' && c8 === 'X') ||
+    (c0 === 'X' && c3 === 'X' && c6 === 'X') ||
+    (c1 === 'X' && c4 === 'X' && c7 === 'X') ||
+    (c2 === 'X' && c5 === 'X' && c8 === 'X') ||
+    (c0 === 'X' && c4 === 'X' && c8 === 'X') ||
+    (c2 === 'X' && c4 === 'X' && c6 === 'X')) {
+      console.log('Player X won')
+      // want to disable all squares after this?
+    } else if ((c0 === 'O' && c1 === 'O' && c2 === 'O') ||
+    (c3 === 'O' && c4 === 'O' && c5 === 'O') ||
+    (c6 === 'O' && c7 === 'O' && c8 === 'O') ||
+    (c0 === 'O' && c3 === 'O' && c6 === 'O') ||
+    (c1 === 'O' && c4 === 'O' && c7 === 'O') ||
+    (c2 === 'O' && c5 === 'O' && c8 === 'O') ||
+    (c0 === 'O' && c4 === 'O' && c8 === 'O') ||
+    (c2 === 'O' && c4 === 'O' && c6 === 'O')) {
+      console.log('Player 0 won')
+      // want to disable all squares after this?
+    } else if ((c0 === 'X' || c0 === 'O') && (c1 === 'X' || c1 === 'O') &&
+    (c2 === 'X' || c2 === 'O') && (c3 === 'X' || c3 === 'O') &&
+    (c4 === 'X' || c4 === 'O') && (c5 === 'X' || c5 === 'O') &&
+    (c6 === 'X' || c6 === 'O') && (c7 === 'X' || c7 === 'O') &&
+    (c8 === 'X' || c8 === 'O')) {
+      console.log('Tied!')
+      // want to disable all squares after this?
+    } else {
+      // do nothing
+    }
   }
-  console.log(gameCellsArray)
-
-  
-
-  // if value(x or o) in index 0,1,2 are the same then someone won
-
-  // over = gameBoard.game.cell.index[0] && gameBoard.game.cell.index[0] === gameBoard.game.cell.index[1] && gameBoard.game.cell.index[0] === gameBoard.game.cell.index[2] ? console.log('true') : console.log('false')
 
   // console.log(gameBoard)
   gameApi
